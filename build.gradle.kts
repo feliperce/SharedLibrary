fec0cf6f40e0a78845f4a1c4e6534fa78a49d413
Felipe Rodrigues <felipe.celestino@valid.com>
2021-03-07 22:33:00 -0300

    data class

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,8 @@ repositories {
 
 val libName = "HNFoundation"
 val libGroup = "com.prof18.hn.foundation"
-val libVersionName = "1.0.0"
-val libVersionCode = 10000
+val libVersionName = "1.0.2"
+val libVersionCode = 10002
 
 group = libGroup
 version = libVersionName
