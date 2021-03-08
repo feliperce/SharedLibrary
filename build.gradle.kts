d954221e705a680d81a8ff4258b42398183429dc
Felipe Rodrigues <felipe.celestino@valid.com>
2021-03-07 21:09:47 -0300

    cocoa implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.*
+import java.text.SimpleDateFormat
+
 plugins {
     kotlin("multiplatform") version "1.4.31"
     id("com.android.application")
@@ -6,6 +9,7 @@ plugins {
 }
 
 val libName = "SharedLibrary"
+val libVersionName = "1.0.0"
 val cocoaDestination = "https://github.com/feliperce/SharedLibrary-Cocoa"
 
 group = "me.felipe"
@@ -117,10 +121,120 @@ kotlin {
                 iosX64().binaries.getFramework(libName, "Debug")
             )
             destinationDir = buildDir.resolve(cocoaDestination)
+            print("Destination DIR: $destinationDir")
             group = libName
             description = "Debug lib for iOS"
             dependsOn("link${libName}DebugFrameworkIosArm64")
             dependsOn("link${libName}DebugFrameworkIosX64")
+        }
+
+        register("universalFrameworkRelease", org.jetbrains.kotlin.gradle.tasks.FatFrameworkTask::class) {
+            baseName = libName
+            from(
+                iosArm64().binaries.getFramework(libName, "Release"),
+                iosX64().binaries.getFramework(libName, "Release")
+            )
+            destinationDir = buildDir.resolve(cocoaDestination)
+            group = libName
+            description = "Create the debug fat framework for iOs"
+            dependsOn("link${libName}ReleaseFrameworkIosArm64")
+            dependsOn("link${libName}ReleaseFrameworkIosX64")
+        }
+
+        // Faz o Build e push para ios DEV
+        register("publishDevFramework") {
+            description = "Publish iOs framweork to the Cocoa Repo"
+
+            project.exec {
+                workingDir = File(cocoaDestination)
+                commandLine("git", "checkout", "develop").standardOutput
+            }
+
+            dependsOn("universalFrameworkDebug")
+
+            doLast {
+                val dir = File("${cocoaDestination}/${libName}.podspec")
+                val tempFile = File("${cocoaDestination}/${libName}.podspec.new")
+
+                val reader = dir.bufferedReader()
+                val writer = tempFile.bufferedWriter()
+                var currentLine: String?
+
+                while (reader.readLine().also { currLine -> currentLine = currLine } != null) {
+                    if (currentLine?.startsWith("s.version") == true) {
+                        writer.write("s.version       = \"${libVersionName}\"" + System.lineSeparator())
+                    } else {
+                        writer.write(currentLine + System.lineSeparator())
+                    }
+                }
+                writer.close()
+                reader.close()
+                val successful = tempFile.renameTo(dir)
+
+                if (successful) {
+
+                    val dateFormatter = SimpleDateFormat("dd/MM/yyyy - HH:mm", Locale.getDefault())
+                    project.exec {
+                        workingDir = File(cocoaDestination)
+                        commandLine("git", "commit", "-a", "-m", "\"New dev release: ${libVersionName}-${dateFormatter.format(Date())}\"").standardOutput
+                    }
+
+                    project.exec {
+                        workingDir = File(cocoaDestination)
+                        commandLine("git", "push", "origin", "develop").standardOutput
+                    }
+                }
+            }
+        }
+
+        // Faz o Build e push para ios MASTER
+        register("publishFramework") {
+            description = "Publish iOs framework to the Cocoa Repo"
+
+            project.exec {
+                workingDir = File(cocoaDestination)
+                commandLine("git", "checkout", "master").standardOutput
+            }
+
+            dependsOn("universalFrameworkRelease")
+
+            doLast {
+                val dir = File("${cocoaDestination}/${libName}.podspec")
+                val tempFile = File("${cocoaDestination}/${libName}.podspec.new")
+
+                val reader = dir.bufferedReader()
+                val writer = tempFile.bufferedWriter()
+                var currentLine: String?
+
+                while (reader.readLine().also { currLine -> currentLine = currLine } != null) {
+                    if (currentLine?.startsWith("s.version") == true) {
+                        writer.write("s.version       = \"${libVersionName}\"" + System.lineSeparator())
+                    } else {
+                        writer.write(currentLine + System.lineSeparator())
+                    }
+                }
+                writer.close()
+                reader.close()
+                val successful = tempFile.renameTo(dir)
+
+                if (successful) {
+
+                    project.exec {
+                        workingDir = File(cocoaDestination)
+                        commandLine("git", "commit", "-a", "-m", "\"New release: ${libVersionName}\"").standardOutput
+                    }
+
+                    project.exec {
+                        workingDir = File(cocoaDestination)
+                        commandLine("git", "tag", libVersionName).standardOutput
+                    }
+
+                    project.exec {
+                        workingDir = File("$rootDir/../../hn-foundation-cocoa")
+                        commandLine("git", "push", "origin", "master", "--tags").standardOutput
+                    }
+                }
+            }
         }
     }
 }
